4f8d26c61032867512337a8d96476887b9c0d519
Ian Davis <me@iandavis.com>
2012-06-10 19:57:31 +0100

    Added license header

--- microdata_test.go
+++ microdata_test.go
@@ -1,3 +1,9 @@
+/*
+  To the extent possible under law, Ian Davis has waived all copyright
+  and related or neighboring rights to this Source Code file.
+  This work is published from the United Kingdom. 
+*/
+
 package microdata
 
 import (
